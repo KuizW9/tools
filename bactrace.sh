35f30755285a28d61ae107d39dcebf4617c63c1f
KuizW9 <kuiz2sw@gmail.com>
2025-06-01 14:14:47 +0800

    Create bactrace.sh

---
+++ bactrace.sh
@@ -0,0 +1,28 @@
+curl nxtrace.org/nt |bash
+
+echo -e "${YELLOW}No:1/9 Traceroute to 中国 广东 电信 (TCP Mode, Max 30 Hop, IPv4)${RESET}"
+nexttrace 14.22.48.9
+
+echo -e "${YELLOW}No:2/9 Traceroute to 中国 上海 电信 (TCP Mode, Max 30 Hop, IPv4)${RESET}"
+nexttrace 101.226.101.195
+
+echo -e "${YELLOW}No:3/9 Traceroute to 中国 北京 电信 (TCP Mode, Max 30 Hop, IPv4)${RESET}"
+nexttrace 106.37.68.26
+
+echo -e "${YELLOW}No:4/9 Traceroute to 中国 广东 联通 (TCP Mode, Max 30 Hop, IPv4)${RESET}"
+nexttrace 157.148.134.3
+
+echo -e "${YELLOW}No:5/9 Traceroute to 中国 上海 联通 (TCP Mode, Max 30 Hop, IPv4)${RESET}"
+nexttrace 112.64.235.107
+
+echo -e "${YELLOW}No:6/9 Traceroute to 中国 北京 联通 (TCP Mode, Max 30 Hop, IPv4)${RESET}"
+nexttrace 61.49.142.114
+
+echo -e "${YELLOW}No:7/9 Traceroute to 中国 广东 移动 (TCP Mode, Max 30 Hop, IPv4)${RESET}"
+nexttrace 221.179.11.205
+
+echo -e "${YELLOW}No:8/9 Traceroute to 中国 上海 移动 (TCP Mode, Max 30 Hop, IPv4)${RESET}"
+nexttrace 117.185.117.117
+
+echo -e "${YELLOW}No:9/9 Traceroute to 中国 北京 移动 (TCP Mode, Max 30 Hop, IPv4)${RESET}"
+nexttrace 211.136.93.48
